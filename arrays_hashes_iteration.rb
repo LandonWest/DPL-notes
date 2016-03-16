fac960ba880a17570a66f30c3784f6e599f88bcc
LandonWest <landonwest5@gmail.com>
2016-03-15 22:16:18 -0700

    add ruby arrays and hashes iteration code examples

---
+++ arrays_hashes_iteration.rb
@@ -0,0 +1,60 @@
+
+def histogram
+  puts "Enter your text to show frequency of each word: "
+  text = gets.chomp
+
+  words = text.downcase.split(" ")
+
+  frequencies = Hash.new(0)
+
+  words.each { |word| frequencies[word] += 1 }
+
+  frequencies = frequencies.sort_by { |word, count| count }.reverse!
+
+  frequencies.each { |word, count| puts word + ": " + count.to_s }
+end
+
+#_______________________________________________________________________________
+#_______________________________________________________________________________
+
+def redact
+  puts "Enter secret message: "
+  text = gets.chomp.downcase
+
+  puts "What word(s) should be REDACTED? use a ',' to separate multiple words"
+  redact = gets.chomp.downcase.split(", ")
+
+  words = text.split(" ")
+
+  redacted_message =
+  words.each do |w|
+      if redact.include? w
+          print "REDACTED "
+      else
+          print w + " "
+      end
+  end
+
+  puts "**begin redacted message** " + redacted_message.to_s + " **end redacted message"
+end
+
+#_______________________________________________________________________________
+#_______________________________________________________________________________
+
+# sort an array, the block uses the combined comparison operator to tell it to sort in descending order
+fruits = %w(orange apple banana pear grapes)
+
+fruits.sort! { |fruit1, fruit2| fruit2 <=> fruit1 }
+
+#_______________________________________________________________________________
+#_______________________________________________________________________________
+
+# custom method to reverse an array alphabetically (or numerically)
+def alphabetize(arr, rev = false)
+    arr.sort!
+    arr.reverse! if rev == true
+end
+
+names = %w( landon eric west john lewis zach )
+
+puts alphabetize(names, rev=true)
